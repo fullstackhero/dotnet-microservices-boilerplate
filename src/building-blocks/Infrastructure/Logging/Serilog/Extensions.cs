68104c66f2b288ab178adcc6b1fb83f26a2464ab
Mukesh Murugan <iammukeshm@gmail.com>
2023-01-22 10:49:13 +0530

    sln cleanup

---
+++ src/building-blocks/Infrastructure/Logging/Serilog/Extensions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FSH.Infrastructure.Logging.Serilog;
+
+public static class Extensions
+{
+    public static RegisterSerilog(IServiceCollection serviceCollection)
+    {
+
+    }
+}
