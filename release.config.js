3072735d3437ccc09fe4a5e36e16df4643e52d47
Mukesh Murugan <iammukeshm@gmail.com>
2023-01-30 20:24:56 +0530

    ci: fix branch

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 const config = {
-    branches: ['feature/semantic-releases'],
+    branches: ['main'],
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
