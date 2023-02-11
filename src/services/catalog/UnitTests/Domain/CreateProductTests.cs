151cd9108821c01d7875265e793524aefa6daa36
Mukesh Murugan <iammukeshm@gmail.com>
2023-02-11 10:51:38 +0530

    test: add more test cases - domain

--- src/services/catalog/UnitTests/Domain/CreateProductTests.cs
+++ src/services/catalog/UnitTests/Domain/CreateProductTests.cs
@@ -6,11 +6,29 @@ namespace Catalog.UnitTests.Domain;
 public class CreateProductTests
 {
     [Fact]
-    public void test_product_creation()
+    public void Product_Create()
     {
+        // Act
         var product = Product.Create("Samsung S22", 1, 20);
+
+        // Assert
         product.Name.Should().Be("Samsung S22");
         product.Quantity.Should().Be(1);
         product.Price.Should().Be(20);
+    }
+
+    [Fact]
+    public void Product_Update()
+    {
+        //Arrange
+        var product = Product.Create("Samsung S22", 1, 20);
+
+        // Act
+        product.Update("Samsung S23", 2, 40);
+
+        // Assert
+        product.Name.Should().Be("Samsung S23");
+        product.Quantity.Should().Be(2);
+        product.Price.Should().Be(40);
     }
 }
