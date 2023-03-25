de1897da59b672addfc2cc9652016f5cc913891c
Mukesh Murugan <iammukeshm@gmail.com>
2023-03-25 17:58:04 +0530

    fix: add code to product dto

--- projects/fluentpos-lite/src/services/catalog/Application/Products/ProductDto.cs
+++ projects/fluentpos-lite/src/services/catalog/Application/Products/ProductDto.cs
@@ -9,6 +9,7 @@ public class ProductDto : IDto
     public Guid Id { get; set; }
     public string? Name { get; set; }
     public string? Slug { get; set; }
+    public string? Code { get; set; }
     public decimal Price { get; set; }
     public decimal AvailableQuantity { get; set; }
 }
