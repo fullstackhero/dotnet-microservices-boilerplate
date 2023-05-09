701d1f4321841129492d1ffe77f0b993b79ef9a2
Mukesh Murugan <iammukeshm@gmail.com>
2023-05-10 00:17:52 +0530

    feat: added minimal apis in cart service

--- fluentpos/services/cart/Api/Controllers/CartsController.cs
+++ fluentpos/services/cart/Api/Controllers/CartsController.cs
@@ -1,56 +1,50 @@
-﻿using FluentPos.Cart.Core;
-using FluentPos.Cart.Core.Dtos;
-using FluentPos.Cart.Core.Features;
-using FSH.Framework.Infrastructure.Controllers;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using System.Net;
+﻿using FSH.Framework.Infrastructure.Controllers;
 
 namespace FluentPos.Cart.Api.Controllers;
 
 public class CartsController : BaseApiController
 {
-    private readonly ILogger<CartsController> _logger;
+    //private readonly ILogger<CartsController> _logger;
 
-    public CartsController(ILogger<CartsController> logger)
-    {
-        _logger = logger;
-    }
+    //public CartsController(ILogger<CartsController> logger)
+    //{
+    //    _logger = logger;
+    //}
 
-    [HttpGet("/{id:guid}", Name = nameof(GetCartAsync))]
-    [Authorize("cart:read")]
-    [ProducesResponseType((int)HttpStatusCode.OK)]
-    [ProducesResponseType((int)HttpStatusCode.BadRequest)]
-    [ProducesResponseType(200, Type = typeof(CustomerCart))]
-    public async Task<IActionResult> GetCartAsync(Guid id)
-    {
-        var query = new GetCart.Query(id);
-        var response = await Mediator.Send(query);
+    //[HttpGet("/{id:guid}", Name = nameof(GetCartAsync))]
+    //[Authorize("cart:read")]
+    //[ProducesResponseType((int)HttpStatusCode.OK)]
+    //[ProducesResponseType((int)HttpStatusCode.BadRequest)]
+    //[ProducesResponseType(200, Type = typeof(CustomerCart))]
+    //public async Task<IActionResult> GetCartAsync(Guid id)
+    //{
+    //    var query = new GetCart.Query(id);
+    //    var response = await Mediator.Send(query);
 
-        return Ok(response);
-    }
+    //    return Ok(response);
+    //}
 
-    [HttpPut("/{id:guid}", Name = nameof(UpdateCartAsync))]
-    [Authorize("cart:write")]
-    [ProducesResponseType((int)HttpStatusCode.OK)]
-    [ProducesResponseType((int)HttpStatusCode.BadRequest)]
-    public async Task<IActionResult> UpdateCartAsync(Guid id, UpdateCartRequestDto updateRequest)
-    {
-        var command = new UpdateCart.Command(updateRequest, id);
-        var response = await Mediator.Send(command);
+    //[HttpPut("/{id:guid}", Name = nameof(UpdateCartAsync))]
+    //[Authorize("cart:write")]
+    //[ProducesResponseType((int)HttpStatusCode.OK)]
+    //[ProducesResponseType((int)HttpStatusCode.BadRequest)]
+    //public async Task<IActionResult> UpdateCartAsync(Guid id, UpdateCartRequestDto updateRequest)
+    //{
+    //    var command = new UpdateCart.Command(updateRequest, id);
+    //    var response = await Mediator.Send(command);
 
-        return Ok(response);
-    }
+    //    return Ok(response);
+    //}
 
-    [HttpPost("/{id:guid}/checkout", Name = nameof(CheckoutCartAsync))]
-    [Authorize("cart:write")]
-    [ProducesResponseType((int)HttpStatusCode.Accepted)]
-    [ProducesResponseType((int)HttpStatusCode.BadRequest)]
-    public async Task<IActionResult> CheckoutCartAsync(Guid id, CheckoutCartRequestDto checkoutRequest)
-    {
-        var command = new CheckoutCart.Command(checkoutRequest, id);
-        await Mediator.Send(command);
+    //[HttpPost("/{id:guid}/checkout", Name = nameof(CheckoutCartAsync))]
+    //[Authorize("cart:write")]
+    //[ProducesResponseType((int)HttpStatusCode.Accepted)]
+    //[ProducesResponseType((int)HttpStatusCode.BadRequest)]
+    //public async Task<IActionResult> CheckoutCartAsync(Guid id, CheckoutCartRequestDto checkoutRequest)
+    //{
+    //    var command = new CheckoutCart.Command(checkoutRequest, id);
+    //    await Mediator.Send(command);
 
-        return Ok();
-    }
+    //    return Ok();
+    //}
 }
--- fluentpos/services/cart/Api/Program.cs
+++ fluentpos/services/cart/Api/Program.cs
@@ -1,6 +1,9 @@
 using FluentPos.Cart.Core;
+using FluentPos.Cart.Core.Dtos;
+using FluentPos.Cart.Core.Features;
 using FSH.Framework.Infrastructure;
 using FSH.Framework.Infrastructure.Auth.OpenId;
+using MediatR;
 
 var coreAssembly = typeof(CartCore).Assembly;
 var builder = WebApplication.CreateBuilder(args);
@@ -10,4 +13,38 @@ builder.Services.AddOpenIdAuth(builder.Configuration, policyNames);
 builder.AddInfrastructure(coreAssembly);
 var app = builder.Build();
 app.UseInfrastructure(builder.Configuration, builder.Environment);
+
+// Get Customer Cart Details
+app.MapGet("/{id:guid}", async (Guid id, ISender _mediatr) =>
+{
+    var query = new GetCart.Query(id);
+    return Results.Ok(await _mediatr.Send(query));
+})
+.RequireAuthorization("cart:read")
+.Produces(200, responseType: typeof(CustomerCart))
+.Produces(400);
+
+
+// Update Customer Cart
+app.MapPut("/{id:guid}", async (Guid id, UpdateCartRequestDto updateRequest, ISender _mediatr) =>
+{
+    var command = new UpdateCart.Command(updateRequest, id);
+    return Results.Ok(await _mediatr.Send(command));
+})
+.RequireAuthorization("cart:write")
+.Produces(200)
+.Produces(400);
+
+
+// Checkout Customer Cart
+app.MapPost("/{id:guid}/checkout", async (Guid id, CheckoutCartRequestDto checkoutRequest, ISender _mediatr) =>
+{
+    var command = new CheckoutCart.Command(checkoutRequest, id);
+    await _mediatr.Send(command);
+    return Results.Ok();
+})
+.RequireAuthorization("cart:write")
+.Produces(202)
+.Produces(400);
+
 app.Run();
