65f49c5d6c1d85430c0732c1a08debcd331f6298
Mukesh Murugan <iammukeshm@gmail.com>
2023-05-18 04:13:24 +0530

    chore: project deps cleanup

--- fluentpos/services/catalog/Api/Program.cs
+++ fluentpos/services/catalog/Api/Program.cs
@@ -1,40 +1,26 @@
-using Dapr;
-using FluentPos.Catalog.Application;
 using FluentPos.Catalog.Infrastructure;
-using FluentPos.Shared.Events;
-using FSH.Framework.Infrastructure;
-using FSH.Framework.Infrastructure.Auth.OpenId;
-using FSH.Framework.Infrastructure.Dapr;
-using FSH.Framework.Persistence.Mongo;
 
-var applicationAssembly = typeof(CatalogApplication).Assembly;
 var builder = WebApplication.CreateBuilder(args);
-
-var policyNames = new List<string> { "catalog:read", "catalog:write" };
-builder.Services.AddOpenIdAuth(builder.Configuration, policyNames);
-
-builder.Services.AddCatalogRepositories();
-builder.Services.AddMongoDbContext<MongoDbContext>(builder.Configuration);
-builder.AddInfrastructure(applicationAssembly);
+builder.AddCatalogInfrastructure();
 var app = builder.Build();
-app.UseInfrastructure(builder.Environment);
+app.UseCatalogInfrastructure();
 
-// this will be invoked when a new product is created
-// this will be handled in some other way
-// keeping it here to demonstrate cross-container/cross-service communication via RMQ topics
-app.MapPost("/handleProductCreation", [Topic(DaprConstants.RMQPubSub, nameof(ProductCreatedEvent))] (ProductCreatedEvent item) =>
-{
-    Console.WriteLine($"Received ProductCreatedEvent Notification - Product Id : {item.ProductId} & ProductName: {item.ProductName}");
-    return Results.Ok();
-});
+//// this will be invoked when a new product is created
+//// this will be handled in some other way
+//// keeping it here to demonstrate cross-container/cross-service communication via RMQ topics
+//app.MapPost("/handleProductCreation", [Topic(DaprConstants.RMQPubSub, nameof(ProductCreatedEvent))] (ProductCreatedEvent item) =>
+//{
+//    Console.WriteLine($"Received ProductCreatedEvent Notification - Product Id : {item.ProductId} & ProductName: {item.ProductName}");
+//    return Results.Ok();
+//});
 
-// this will be invoked when a cart is checked out using the cart/{customerId}/checkout [POST] endpoint.
-// this will be moved to order api later on
-app.MapPost("/handleCheckout", [Topic(DaprConstants.RMQPubSub, nameof(CartCheckedOutEvent))] (CartCheckedOutEvent @event) =>
-{
-    Console.WriteLine($"Received CartCheckedOutEvent Notification - Customer Id : {@event.CustomerId} & Credit Card Number: {@event.CreditCardNumber}");
-    return Results.Ok();
-});
+//// this will be invoked when a cart is checked out using the cart/{customerId}/checkout [POST] endpoint.
+//// this will be moved to order api later on
+//app.MapPost("/handleCheckout", [Topic(DaprConstants.RMQPubSub, nameof(CartCheckedOutEvent))] (CartCheckedOutEvent @event) =>
+//{
+//    Console.WriteLine($"Received CartCheckedOutEvent Notification - Customer Id : {@event.CustomerId} & Credit Card Number: {@event.CreditCardNumber}");
+//    return Results.Ok();
+//});
 
 app.MapGet("/", () => "Hello From Catalog Service").AllowAnonymous();
 app.Run();
--- fluentpos/services/catalog/Infrastructure/Extensions.cs
+++ fluentpos/services/catalog/Infrastructure/Extensions.cs
@@ -1,12 +1,26 @@
-﻿using FluentPos.Catalog.Application.Products;
+﻿using FluentPos.Catalog.Application;
+using FluentPos.Catalog.Application.Products;
 using FluentPos.Catalog.Infrastructure.Repositories;
+using FSH.Framework.Infrastructure;
+using FSH.Framework.Infrastructure.Auth.OpenId;
+using FSH.Framework.Persistence.Mongo;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FluentPos.Catalog.Infrastructure;
 public static class Extensions
 {
-    public static void AddCatalogRepositories(this IServiceCollection services)
+    public static void AddCatalogInfrastructure(this WebApplicationBuilder builder)
     {
-        services.AddTransient<IProductRepository, ProductRepository>();
+        var applicationAssembly = typeof(CatalogApplication).Assembly;
+        var policyNames = new List<string> { "catalog:read", "catalog:write" };
+        builder.Services.AddOpenIdAuth(builder.Configuration, policyNames);
+        builder.AddInfrastructure(applicationAssembly);
+        builder.Services.AddMongoDbContext<MongoDbContext>(builder.Configuration);
+        builder.Services.AddTransient<IProductRepository, ProductRepository>();
+    }
+    public static void UseCatalogInfrastructure(this WebApplication app)
+    {
+        app.UseInfrastructure(app.Environment);
     }
 }
